bd45c7f4d7b5b0fe1aed51260bb140999e78c241
LiefTheBeef <32721304+LiefTheBeef@users.noreply.github.com>
2019-02-04 23:55:15 -0800

    Aljon's Level changes, plz update your unity btw

--- Loose/Assets/Scripts/MovementAndLook.cs
+++ Loose/Assets/Scripts/MovementAndLook.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class MovementAndLook : MonoBehaviour {
 
     public float lookSensitivity;
-    public float moveSpeed;
+    private float _currentMoveSpeed;
     public float normalMoveSpeed;
     public float sprintingSpeed;
     
@@ -24,39 +24,35 @@ public class MovementAndLook : MonoBehaviour {
 	void Update () {
         if(Input.GetKeyDown(KeyCode.LeftShift))
         {
-            moveSpeed = sprintingSpeed;
+            _currentMoveSpeed = sprintingSpeed;
         }
         else if(Input.GetKeyUp(KeyCode.LeftShift))
         {
-            moveSpeed = normalMoveSpeed;
+            _currentMoveSpeed = normalMoveSpeed;
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             this.GetComponent<Rigidbody>().AddForce(0, 500, 0);
         }
-        look();
-
-
+        Look();
 
 
     }
     private void FixedUpdate()
     {
-        move();
+        Move();
     }
 
-    void move()
+    void Move()
     {
         Vector3 moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        moveDir = transform.TransformDirection(moveDir);
-        moveDir *= moveSpeed * Time.deltaTime * 14;
+        moveDir = this.transform.TransformDirection(moveDir);
+        moveDir *= _currentMoveSpeed * Time.deltaTime * 14;
 
         this.GetComponent<Rigidbody>().AddForce(moveDir, ForceMode.VelocityChange);
-
-        //this.gameObject.GetComponent<Rigidbody>().MovePosition(this.gameObject.transform.position + moveDir);
     }
 
-    void look()
+    void Look()
     {
         float scaledMouseDeltaX = Input.GetAxis("Mouse X") * lookSensitivity;
         float scaledMouseDeltaY = Input.GetAxis("Mouse Y") * lookSensitivity;
