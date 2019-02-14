de3b5e437b5418dbb8fe6cd5881de32c51a394de
gavindent <gavindent@hotmail.com>
2019-02-13 18:43:53 -0800

    enemy siege towers should no longer spawn on top of each other

--- Loose/Assets/Scripts/EnemyController.cs
+++ Loose/Assets/Scripts/EnemyController.cs
@@ -22,11 +22,21 @@ public class EnemyController : MonoBehaviour {
     {
         float randomDir = Random.Range(0, 359);
         Vector3 spawnLocation = new Vector3(Mathf.Cos(randomDir), 0, Mathf.Sin(randomDir)) * 400;
-
+        while (CheckForSpawn(spawnLocation))
+        {
+            randomDir = Random.Range(0, 359);
+            spawnLocation = new Vector3(Mathf.Cos(randomDir), 0, Mathf.Sin(randomDir)) * 400;
+        }
         GameObject newSiegeTower = GameObject.Instantiate(siegeTowerPrefab, spawnLocation, Quaternion.identity);
         newSiegeTower.transform.rotation = Quaternion.Slerp(newSiegeTower.transform.rotation
             , Quaternion.LookRotation(new Vector3(0,0, 0) - newSiegeTower.transform.position), 1f);
         newSiegeTower.transform.Translate(0, 0, 0);
 
+    }
+
+    private bool CheckForSpawn(Vector3 location)
+    {
+        Vector3 tempSpawn = new Vector3(location.x, location.y + 31.0f, location.z);
+        return Physics.CheckSphere(tempSpawn, 30.0f);
     }
 }
