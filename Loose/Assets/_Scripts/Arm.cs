539b1871be61de6c1d40ce6ece14d1f4612cdb27
Shaun Prince <prince.a.shaun@gmail.com>
2019-03-13 23:01:59 -0700

    Fixed bucket model, fixed castle model in shaun scene,catapult fire and reload are working, but turning and angle are not

---
+++ Loose/Assets/_Scripts/Arm.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arm : MonoBehaviour {
+
+    public float xTorque;
+
+    public BoxCollider[] bucketColliders;
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.GetComponent<Rigidbody>().centerOfMass = this.gameObject.transform.localPosition;
+        LowerBucketEdges();
+        //this.gameObject.GetComponent<Rigidbody>().AddRelativeTorque(xTorque,0,0,ForceMode.VelocityChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LowerBucketEdges()
+    {
+        for(int i = 0; i < 4; ++i)
+        {
+            bucketColliders[i].size = new Vector3(bucketColliders[i].size.x, bucketColliders[i].size.y, bucketColliders[i].size.z * .5f);
+        }
+    }
+
+    public void RaiseBucketEdges()
+    {
+        for (int i = 0; i < 4; ++i)
+        {
+            bucketColliders[i].size = new Vector3(bucketColliders[i].size.x, bucketColliders[i].size.y, bucketColliders[i].size.z * 2f);
+        }
+    }
+}
